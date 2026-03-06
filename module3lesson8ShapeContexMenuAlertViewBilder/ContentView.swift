a6fa23c02ff9d622529b7a6846f0b7899c85e1eb
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-03-06 08:22:35 +0300

    Initial Commit

---
+++ module3lesson8ShapeContexMenuAlertViewBilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  module3lesson8ShapeContexMenuAlertViewBilder
+//
+//  Created by Максим Минаков on 06.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ module3lesson8ShapeContexMenuAlertViewBilder/module3lesson8ShapeContexMenuAlertViewBilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson8ShapeContexMenuAlertViewBilderApp.swift
+//  module3lesson8ShapeContexMenuAlertViewBilder
+//
+//  Created by Максим Минаков on 06.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson8ShapeContexMenuAlertViewBilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
